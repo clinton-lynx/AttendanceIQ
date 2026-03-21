4a313c1ff254738161559f07b2cadd3bb97ef6d1
clinton-lynx <clintonadeoti1@gmail.com>
2026-03-21 12:02:27 +0100

    feat: added get attendance  endpoint

--- app.py
+++ app.py
@@ -1,41 +1,3 @@
-# from flask import Flask, jsonify
-# from flask_cors import CORS
-
-# import config
-# from routes.auth import auth_bp
-# from routes.students import students_bp
-# from routes.sessions import sessions_bp
-# from routes.attendance import attendance_bp
-
-# def create_app():
-#     app = Flask(__name__)
-    
-#     # Enable Cross-Origin Resource Sharing
-#     CORS(app)
-
-#     # Register Blueprints
-#     app.register_blueprint(auth_bp, url_prefix='/api/auth')
-#     app.register_blueprint(students_bp, url_prefix='/api/students')
-#     app.register_blueprint(sessions_bp, url_prefix='/api/sessions')
-#     app.register_blueprint(attendance_bp, url_prefix='/api/attendance')
-
-#     @app.route('/')
-#     def index():
-#         return jsonify({
-#             "success": True, 
-#             "message": "AttendanceIQ API is Running",
-#             "version": "1.0.0"
-#         })
-
-#     @app.errorhandler(404)
-#     def not_found(e):
-#         return jsonify({"success": False, "message": "Endpoint not found"}), 404
-
-#     return app
-
-# if __name__ == '__main__':
-#     app = create_app()
-#     app.run(host=config.HOST, port=config.PORT, debug=True)
 
 
 
@@ -44,12 +6,18 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 import config
 from routes.students import students_bp
+from routes.auth import auth_bp
+from routes.sessions import sessions_bp
+from routes.attendance import attendance_bp
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
 
+    app.register_blueprint(auth_bp, url_prefix='/api/auth')
     app.register_blueprint(students_bp, url_prefix='/api/students')
+    app.register_blueprint(sessions_bp, url_prefix='/api/sessions')
+    app.register_blueprint(attendance_bp, url_prefix='/api/attendance')
 
     @app.route('/')
     def index():
